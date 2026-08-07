2aa5d3a8001b72e064ad88aaf26a2d4217db0c30
cesia <cesiam@mit.edu>
2026-08-06 21:51:48 -0400

    chunk file has cleaned metadata and associates the chunks with the docs

--- mpc_rag_server/src/ingestion/chunk.py
+++ mpc_rag_server/src/ingestion/chunk.py
@@ -4,20 +4,24 @@ from convert import docling_docs
 from docling.chunking import HybridChunker
 from docling_core.transforms.chunker.tokenizer.huggingface import HuggingFaceTokenizer
 from transformers import AutoTokenizer
+from uuid import uuid4
 
-
-EMBED_MODEL_ID = 'nomic-ai/nomic-embed-text-v1.5'
 
 tokenizer = HuggingFaceTokenizer(
-    tokenizer=AutoTokenizer.from_pretrained(EMBED_MODEL_ID),
+    tokenizer=AutoTokenizer.from_pretrained("sentence-transformers/all-MiniLM-L6-v2"),
+    max_tokens=384,
 )
 chunker = HybridChunker(tokenizer=tokenizer)
-chunked_docs = []
+recorded_chunks = []
 for doc in docling_docs:
-    doc_uuid = None
-    for chunk in chunker.chunk(doc):
-        chunk_uuid = None
-        chunk.metadata['doc_uuid'] = doc_uuid
-        chunk.metadata['chunk_uuid'] = chunk_uuid  
-        chunked_docs.append(chunk)
+    doc_uuid = str(uuid4())
+    for chunk in chunker.chunk_document(doc):
+        recorded_chunks.append({
+            "chunk_uuid": str(uuid4()),
+            "doc_uuid": doc_uuid,
+            "text": chunk.text,
+            "headings": chunk.meta.headings,
+            "page_no": chunk.meta.doc_items[0].prov[0].page_no if chunk.meta.doc_items and chunk.meta.doc_items[0].prov else None,
+            "filename": chunk.meta.origin.filename if chunk.meta.origin else None,
+        })
 
